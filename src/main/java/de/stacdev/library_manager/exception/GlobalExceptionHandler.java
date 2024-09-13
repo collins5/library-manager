f041bd97399574fdf18945898519795a97d74eab
Collins Audrey Souop Tagne <t.stacdev@gmail.com>
2024-09-13 10:17:26 +0200

    GlobalExceptionHandler modified

--- src/main/java/de/stacdev/library_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/de/stacdev/library_manager/exception/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Collins Souop
  */
@@ -13,8 +16,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFound(UserNotFoundException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error ", ex.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+                .body(errorResponse);
     }
 }
